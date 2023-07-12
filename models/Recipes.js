8fc3f0ba4541a5ac6e57ec351d5064cf1fd99baf
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2023-07-12 10:23:11 -0600

    MODIFICACION DE RECIPIES-JS

--- models/Recipes.js
+++ models/Recipes.js
@@ -2,13 +2,30 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const recipesSchema = new Schema({
-  nombre: {
+  name: {
     type: String,
     trim: true,
     tolowercase: true,
     required: true,
   },
-  // TODO: FALTA TERMINAR EL MODELO.
+  ingredients: [
+    {
+      type: String,
+      tolowercase: true,
+      required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+  ],
+  link: {
+    type: String,
+    trim: true,
+  },
+  picture: {
+    type: String,
+    trim: true,
+  },
 });
 
 module.exports = mongoose.model("Recipes", recipesSchema);
